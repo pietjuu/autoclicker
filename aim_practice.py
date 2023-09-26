9a8435574bd65c45698c971d9d3d1112c377bfd3
Pieter Verheye <pieter.verheye@student.howest.be>
2023-09-26 23:12:08 +0200

    init commit + open website

---
+++ aim_practice.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Import modules
+import pyautogui
+import time
+import webbrowser
+
+# Open the web browser
+webbrowser.open("https://humanbenchmark.com/tests/aim", new=2)
+time.sleep(5)  # time to open the browser

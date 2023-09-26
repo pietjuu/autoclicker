413eeecdf2fda5ef96ef15c170286fb0b5e6fc7e
Pieter Verheye <pieter.verheye@student.howest.be>
2023-09-26 23:02:35 +0200

    refactor code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 # Import modules
 import pyautogui
 import time
@@ -18,6 +17,7 @@ levels = 5
 print(width, height)
 width = int(width / 2)
 height = int(height / 4)
+
 pyautogui.moveTo(width, height)
 
 # gets the current hex color of the game
@@ -25,14 +25,11 @@ color = pyautogui.pixel(width, height)
 hexColor = '%02x%02x%02x' % color
 print(hexColor)
 if hexColor == '2b87d1':
-    print("test")
     pyautogui.click()
     for x in range(levels):
         # checks every time if the color is not green,
         # when it is green it delays and then clicks the screen and waits for the next round
         while hexColor != '4bdb6a':
-            print("test while loop")
-            print(hexColor)
             color = pyautogui.pixel(width, height)
             hexColor = '%02x%02x%02x' % color
             print("test na calculatie")

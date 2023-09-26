8416ce0497bd2c24297772aaa2a43f14a6f61bdd
Pieter Verheye <pieter.verheye@student.howest.be>
2023-09-26 20:32:53 +0200

    added openwebbrowser

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# Import modules
+import pyautogui
+import time
+import webbrowser
+
+# Open the web browser
+webbrowser.open('https://www.humanbenchmark.com/tests/reactiontime', new=2)
+time.sleep(10)  # time to open the webbrowser
+
+

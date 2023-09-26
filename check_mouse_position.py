1dd32e445e5862f7709696e3b38f6ed2edddde1d
Pieter Verheye <pieter.verheye@student.howest.be>
2023-09-26 20:30:21 +0200

    init commit + check_mouse_position

---
+++ check_mouse_position.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+try:
+    while True:
+        # Get the current position of the mouse cursor
+        current_position = pyautogui.position()
+
+        # Print the cursor's x and y coordinates
+        print("Mouse Cursor Position: x =", current_position[0], ", y =", current_position[1])
+
+except KeyboardInterrupt:
+    # Exit the loop when you press Ctrl+C
+    pass

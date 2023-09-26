410b55f124d08be162c6eaacffbf64d988208cd8
Pieter Verheye <pieter.verheye@student.howest.be>
2023-09-26 20:36:36 +0200

    calculate screensize and print it

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ import webbrowser
 
 # Open the web browser
 webbrowser.open('https://www.humanbenchmark.com/tests/reactiontime', new=2)
-time.sleep(10)  # time to open the webbrowser
+time.sleep(10)  # time to open the browser
 
+# variables
+delay = 0
+levels = 5
+
+# calculate screensize and position to click
+(width, height) = pyautogui.size()
+print(width, height)
+width = int(width / 2)
+height = int(height / 4)
+pyautogui.moveTo(width, height)
 

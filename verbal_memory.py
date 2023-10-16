51237bad8341733c398c29a5c470d51e34bebed1
Pieter Verheye <pieter.verheye@student.howest.be>
2023-10-16 15:21:01 +0200

    made the script work without failsafe

--- verbal_memory.py
+++ verbal_memory.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # Import modules
-import pyautogui
 import time
+import pyautogui
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from pynput.mouse import Button, Controller
@@ -16,23 +16,48 @@ driver.get(url)
 # Wait for the page to load
 time.sleep(5)
 
-# get the page source
-html = driver.page_source
 
 # create a BeautifulSoup object
-soup = BeautifulSoup(html, "html.parser")
-spans = soup.find_all("div", class_="word")
+def get_soup(driver):
+    return BeautifulSoup(driver.page_source, "html.parser")
+
 
 # Wait some time to prevent that weird stuff happens
 time.sleep(1)
 
+# click the text box
+mouse = Controller()
+mouse.position = (947, 735)
+mouse.click(Button.left, 1)
+time.sleep(3)
+
+# Get the current position of the mouse cursor
+current_position = pyautogui.position()
+
+# Print the cursor's x and y coordinates
+print("Mouse Cursor Position: x =", current_position[0], ", y =", current_position[1])
+print("clicked start")
+
 # add word to set, if word is already in set, click "SEEN", else click "NEW"
 seen_words = set()
-for span in spans:
-    word = span.get_text()
-    if word in seen_words:
-        pyautogui.click(1071, 524)
+print(seen_words)
+while True:
+    # Re-fetch the spans inside the loop to reflect the updated content
+    soup = get_soup(driver)
+    spans = soup.find_all("div", class_="word")
+
+    if spans:
+        word = spans[0].get_text()
+
+        if word in seen_words:
+            mouse.position = (868, 625)
+            mouse.click(Button.left, 1)
+            print(seen_words)
+        else:
+            mouse.position = (1031, 625)
+            mouse.click(Button.left, 1)
+            seen_words.add(word)
+            print(seen_words)
+        time.sleep(0.1)
     else:
-        pyautogui.click(1071, 524)
-        seen_words.add(word)
-    time.sleep(0.1)
+        break

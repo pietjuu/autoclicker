939c6b949666936aafbbf99fe44157b3a4d0f3f8
Pieter Verheye <pieter.verheye@student.howest.be>
2023-10-17 22:29:41 +0200

    fixed the program by changing logic to state machine

--- humanBenchMark/number_memory.py
+++ humanBenchMark/number_memory.py
@@ -28,6 +28,7 @@ def get_soup(driver):
     return BeautifulSoup(driver.page_source, "html.parser")
 
 
+"""
 def check_if_number_is_on_the_screen():
     soup = get_soup(driver)
     divs = soup.find_all("div", class_="big-number")
@@ -37,6 +38,15 @@ def check_if_number_is_on_the_screen():
     else:
         print("number is still there")
         return False
+
+"""
+
+
+def get_number():
+    soup = get_soup(driver)
+    divs = soup.find_all("div", class_="big-number")
+    number = "".join([div.get_text() for div in divs])
+    return number
 
 
 # Wait some time to prevent that weird stuff happens
@@ -54,27 +64,33 @@ max_Write = 250
 # TODO check if number is still there, when it is gone write the number down (check html)
 while write_count < max_Write:
     # Re-fetch the spans inside the loop to reflect the updated content
+    """
     soup = get_soup(driver)
     divs = soup.find_all("div", class_="big-number")
     number = "".join([div.get_text() for div in divs])
+    """
+    # state 1
+    number = ""
+    while number in ["", " "]:
+        number = get_number()
 
-    while not check_if_number_is_on_the_screen():
-        if number:
-            print(number)
-            pyautogui.typewrite(number, interval=0)
+    # state 2
+    while get_number() not in ["", " "]:
+        pass
 
-            time.sleep(2)
-            mouse.position = (943, 674)
-            mouse.click(Button.left, 1)
-            time.sleep(2)
+    # state 3
+    #time.sleep(0.1)
+    pyautogui.typewrite(number, interval=0)
 
-            mouse.position = (945, 756)
-            mouse.click(Button.left, 1)
-            time.sleep(2)
-            write_count += 1
-            number = ""
-        else:
-            print("number is gone")
+    #time.sleep(0.1)
+    mouse.position = (943, 674)
+    mouse.click(Button.left, 1)
+    #time.sleep(0.1)
 
+    mouse.position = (945, 756)
+    mouse.click(Button.left, 1)
+    #time.sleep(0.1)
+    write_count += 1
+    number = ""
 
 input("Press Enter to continue...")
